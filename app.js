e3396d5a797c4a20f8e0db2f9d940aad7b864238
tarif.m <tarif.m@zohocorp.com>
2023-10-26 22:52:11 +0530

    fragments

--- app.js
+++ app.js
@@ -27,18 +27,72 @@
 //      )
 // }
 
+var ages = [10,20,30, 40, 50];
+
+
 
 var MyComponent = ()=>{
-       return <>
+       
+    return <>
            <h1 className="heading">This is a heading</h1>
-           <p>This is a paragraph </p>
+           <ul>
+            {ages.map(
+                (ele, index)=>{
+                    return <li key={index}>{ele}</li>
+                }
+            )}
+           </ul>
        </>
 }
 
-function Component2(){
-      return  <>
-                <h1 className="heading">This is a heading</h1>
-                <p>This is a paragraph </p>
-            </>
+var GodsComponent = ()=>{
+    var ages = [
+        {
+            name: 'Ram Ji',
+            age: '26',
+            address: 'Ayodhya'
+        },{
+            name: 'Seeta Ji',
+            age: '20',
+            address: 'Nepal'
+        },{
+            name: 'Radha',
+            age: '17',
+            address: 'Mathura'
+        }
+     ];
+     return (
+        <>
+            <table>
+              <thead>
+              <tr>
+                    <th>Name</th>
+                    <th>Age</th>
+                    <th>Address</th>
+                </tr>
+              </thead>
+                <tbody>
+                {ages.map(
+                    (e, i)=>{
+                        return (
+                            <tr key={i}>
+                                <td>{e.name}</td>
+                                <td>{e.age}</td>
+                                <td>{e.address}</td>
+                            </tr>
+                        )
+                    }
+                )}
+                </tbody>
+            </table>
+        </>
+     )
 }
-ReactDOM.createRoot(document.getElementById('container')).render(<><MyComponent/> <Component2/> </>);
+
+var myReact = ReactDOM.createRoot(document.getElementById('container'));
+myReact.render(
+    <>
+        <GodsComponent />
+        <MyComponent />
+    </>
+);
